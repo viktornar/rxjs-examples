623d4b141ef5ea79bf631cb68de844d546362d36
viktornar <vnareiko.lt@gmail.com>
2023-01-26 10:34:15 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { of, map, Observable, fromEvent } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+const CLICK_EVT_NAME = 'click';
 
-// Open the console in the bottom right to see results.
+// Creation observable examples
+const exampleOf$ = of('World').pipe(map((name) => `Hello, ${name}!`));
+
+exampleOf$.subscribe(console.log);
+
+const exampleFromEvent$ = fromEvent(
+  document.getElementById('example-1'),
+  CLICK_EVT_NAME
+);
+
+exampleFromEvent$.subscribe((evt) => {
+  alert('Hello world');
+});
